f396f9886ecec7bfbb7c2a5f4894a23d949affeb
Alexander Eckert <aeckert@alaska.edu>
2017-03-22 16:08:44 -0800

    created cps.cpp/cps.h

---
+++ cps.h
@@ -0,0 +1,10 @@
+// cps.h
+//
+// created by
+// Frank Cline	(fmcline@alaska.edu)
+// Alexander Eckert (aeckert@alaska.edu)
+// Jeremiah Jacobson (jjjacobson2@alaska.edu)
+// Rohan Weeden (reweeden@alaska.edu)
+// 3-22-2017
+// Project 2 for CS372 Spring 2017 at UAF
+// Professor Dr. Hartman
